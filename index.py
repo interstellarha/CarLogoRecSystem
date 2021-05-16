88f88f8b749358f50cf1e53f994c77b0c779f3b5
DetVanderdeer <62175751+DetVanderdeer@users.noreply.github.com>
2021-05-16 11:42:48 +0800

    车诗琪：欢迎页面逻辑，待完善
    需要添加按钮事件等

---
+++ index.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time: 2021/5/16 11:14
+# @Author: 车诗琪
+# @File: index
+# @Project: HWprogram
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget
+
+from index_ui import Ui_index
+
+
+class FormIndex(QWidget, Ui_index):
+    def __init__(self, parent=None):
+        super(FormIndex, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dialog = FormIndex()
+    dialog.show()
+    sys.exit(app.exec_())
